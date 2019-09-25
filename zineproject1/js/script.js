27124d5f389ff9fc5959de380bbb22dd59be635d
SEOJU-JUNG <47016765+SEOJU-JUNG@users.noreply.github.com>
2019-09-24 19:39:58 -0700

    e

---
+++ zineproject1/js/script.js
@@ -0,0 +1,18 @@
+var printText = $('.text1').data('text1');
+
+var contentArray = printText.split('/n');
+$.each(contentArray, function(index, newLine) {
+  $('.text').append('<span style="display:block;" id="'+index+'"></span>');
+
+  var lineID = index;
+  var self = $(this);
+    setTimeout(function () {
+      $.each(self, function(index, chunk){
+          setTimeout(function () {
+            $('#'+lineID).append("<span>"+chunk+"</span>");
+            $('body, html').scrollTop($(document).height());
+          }, index*5);
+      });
+
+    }, index*100);
+});
