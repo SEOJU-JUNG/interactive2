3f06438b52e66995cef1f9da6cca838f68ceb147
SEOJU-JUNG <47016765+SEOJU-JUNG@users.noreply.github.com>
2019-10-01 05:45:57 -0700

    d

--- zineproject1/js/script.js
+++ zineproject1/js/script.js
@@ -16,3 +16,19 @@ $.each(contentArray, function(index, newLine) {
 
     }, index*100);
 });
+
+var{
+  font-family: 'Press Start 2P', cursive;
+  position: absolute;
+  left: 50%;
+  margin-left: calc(-1000px/2);
+/* top */
+  margin-top: 320px;
+  width: 1000px;
+  height: 100px;
+  display: inline;
+  text-align: center;
+  font-size: 1.9em;
+  font-weight: normal;
+  float: right;
+}
