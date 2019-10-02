5d72702c04ff5dc7b8dc97292fece0866571d278
SEOJU-JUNG <47016765+SEOJU-JUNG@users.noreply.github.com>
2019-10-02 03:55:39 -0700

    E

--- zineproject1/js/script.js
+++ zineproject1/js/script.js
@@ -17,18 +17,59 @@ $.each(contentArray, function(index, newLine) {
     }, index*100);
 });
 
-var{
-  font-family: 'Press Start 2P', cursive;
-  position: absolute;
-  left: 50%;
-  margin-left: calc(-1000px/2);
-/* top */
-  margin-top: 320px;
-  width: 1000px;
-  height: 100px;
-  display: inline;
-  text-align: center;
-  font-size: 1.9em;
-  font-weight: normal;
-  float: right;
+$(function() {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+         $(‘body’).addClass(‘changeColor’)
+      }
+      if ($(this).scrollTop() < 50) {
+         $(‘body’).removeClass(‘changeColor’)
+      }
+   });
+});
+
+var intervalID = window.setInterval(updateScreen, 200);
+var c = document.getElementById("console");
+
+var txt = [
+  "FORCE: XX0022. ENCYPT://000.222.2345",
+  "TRYPASS: ********* AUTH CODE: ALPHA GAMMA: 1___ PRIORITY 1",
+  "RETRY: REINDEER FLOTILLA",
+  "Z:> /FALKEN/GAMES/TICTACTOE/ EXECUTE -PLAYERS 0",
+  "================================================",
+  "Priority 1 // local / scanning...",
+  "scanning ports...",
+  "BACKDOOR FOUND (23.45.23.12.00000000)",
+  "BACKDOOR FOUND (13.66.23.12.00110000)",
+  "BACKDOOR FOUND (13.66.23.12.00110044)",
+  "...",
+  "...",
+  "BRUTE.EXE -r -z",
+  "...locating vulnerabilities...",
+  "...vulnerabilities found...",
+  "MCP/> DEPLOY CLU",
+  "SCAN: __ 0100.0000.0554.0080",
+  "SCAN: __ 0020.0000.0553.0080",
+  "SCAN: __ 0001.0000.0554.0550",
+  "SCAN: __ 0012.0000.0553.0030",
+  "SCAN: __ 0100.0000.0554.0080",
+  "SCAN: __ 0020.0000.0553.0080",
+]
+
+var docfrag = document.createDocumentFragment();
+
+function updateScreen() {
+  //Shuffle the "txt" array
+  txt.push(txt.shift());
+  //Rebuild document fragment
+  txt.forEach(function(e) {
+    var p = document.createElement("p");
+    p.textContent = e;
+    docfrag.appendChild(p);
+  });
+  //Clear DOM body
+  while (c.firstChild) {
+    c.removeChild(c.firstChild);
+  }
+  c.appendChild(docfrag);
 }
