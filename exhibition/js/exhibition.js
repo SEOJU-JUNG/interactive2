9d10da3be883a16c1e096849c1c28bb2350836c3
SEOJU-JUNG <47016765+SEOJU-JUNG@users.noreply.github.com>
2019-11-22 00:26:47 -0800

    sdf

---
+++ exhibition/js/exhibition.js
@@ -0,0 +1,29 @@
+window.addEventListener('resize',pageResized)
+
+
+function pageResized(){
+
+  let windowPercentOfScreen = (1 - (window.innerHeight / window.screen.height)) * 100
+
+  let colorFromScreenHeight = 'hsl(0,0%,' + windowPercentOfScreen + '%)'
+  document.body.style.color = colorFromScreenHeight
+
+}
+
+pageResized()
+
+
+
+let page01Link = document.querySelector('#page01Link')
+
+page01Link.addEventListener('click',function(event){
+  event.preventDefault()
+
+  document.body.classList.add('pageChangeAnimation')
+
+  setTimeout(function(){
+    window.location.href = page01Link.href
+  },1000)
+
+  return false
+})
---
+++ exhibition/js/page01.js
@@ -0,0 +1,5 @@
+let image = document.querySelector('.img-page01-1')
+
+image.addEventListener('click',function(){
+  image.classList.toggle('clicked')
+})
