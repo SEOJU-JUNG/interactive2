b7387933d7bd8c03d6d75ff275a81cb195e1053f
SEOJU-JUNG <47016765+SEOJU-JUNG@users.noreply.github.com>
2019-12-03 08:49:28 -0800

    ㄴㅇㄹ

--- campaign/js/campaign.js
+++ campaign/js/campaign.js
@@ -1,27 +1,25 @@
-
-
-
-
-
-
-
-    var vip3= 870; //스크롤 위치값
-    $(window).scroll(function(){
-      var num=$(window).scrollTop();
-        if(num>vip3){
-          $(".p3fpart").fadeIn();
-        }else{
-          $(".p3fpart").fadeOut();
-        }
-      });
-
-
-  var vip4= 1600; //스크롤 위치값
-	$(window).scroll(function(){
-		var num=$(window).scrollTop();
-			if(num>vip4){
-				$(".timeline,.introfire,.sum1").fadeIn();
-			}else{
-				$(".timeline,.introfire,.sum1").fadeOut();
-			}
-		});
+  // 
+  //
+  //
+  //
+  //
+  //   var vip3= 870; //스크롤 위치값
+  //   $(window).scroll(function(){
+  //     var num=$(window).scrollTop();
+  //       if(num>vip3){
+  //         $(".p3fpart").fadeIn();
+  //       }else{
+  //         $(".p3fpart").fadeOut();
+  //       }
+  //     });
+  //
+  //
+  // var vip4= 1600; //스크롤 위치값
+	// $(window).scroll(function(){
+	// 	var num=$(window).scrollTop();
+	// 		if(num>vip4){
+	// 			$(".timeline,.introfire,.sum1").fadeIn();
+	// 		}else{
+	// 			$(".timeline,.introfire,.sum1").fadeOut();
+	// 		}
+	// 	});
